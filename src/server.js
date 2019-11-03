f001040d483b7ac8cb9d80cd397d30139b28d7d0
unknown <brener.quevedo@ccc.ufcg.edu.br>
2019-11-03 19:16:03 -0300

    corringindo alguns erros em server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const corts = require("cors");
+const cors = require("cors");
 
 const routes = require("./routes");
 
@@ -18,6 +18,5 @@ mongoose.connect("mongodb+srv://brener:brener123@omnistack-sm6zk.mongodb.net/omn
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(3333);
