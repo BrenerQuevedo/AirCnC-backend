a7f24d186c644f20a4bc6ab35756ce26cc1ee04e
BrenerQuevedo <39017457+BrenerQuevedo@users.noreply.github.com>
2020-01-25 00:12:26 -0300

    estruturando as solicitações de spots

--- src/controllers/ApprovalController.js
+++ src/controllers/ApprovalController.js
@@ -4,9 +4,10 @@ module.exports = {
     async store(req, res) {
         const {booking_id} = req.params;
 
-        const booking = await Booking.findById(booking_id).populated('spot');
+        const booking = await Booking.findById(booking_id).populate('spot');
 
-        booking.approved = true;
+        //booking.approved = (booking.approved == null ? true: res.status(403).json({'error': 'Spot already in use'}));
+    
 
         await booking.save();
 
--- src/controllers/RejectionController.js
+++ src/controllers/RejectionController.js
@@ -1,5 +1,17 @@
+const Booking = require('../models/Booking');
+
 module.exports = {
     async store(req, res) {
-        return res.json({ ok: true});
+        const {booking_id} = req.params;
+
+        const booking = await Booking.findById(booking_id).populate('spot');
+        
+        //console.log(booking.spot.user);
+       //booking.approved = (booking.approved == null ? false: res.status(403).json({'error': 'Spot already in use'}));
+       
+
+        await booking.save();
+
+        return res.json(booking);
     }
 };
